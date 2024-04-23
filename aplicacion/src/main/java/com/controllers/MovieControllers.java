f1998023a5575105106a476ed49e81b6e6b915ca
carlosycl <cycortes1701@gmail.com>
2024-04-22 22:44:18 -0500

    Clase controllers

---
+++ aplicacion/src/main/java/com/controllers/MovieControllers.java
@@ -0,0 +1,11 @@
+package com.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController //controlador que va a estar destinado a resolver las peticiones en formato api rest
+@RequestMapping("api/movies") //mepea todas la peticiones que llegan al sitio api movies 
+
+public class MovieControllers {
+    
+}
