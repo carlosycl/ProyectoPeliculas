d115a1a3460f38e7bd2c8bd74a77d385263c0405
carlosycl <cycortes1701@gmail.com>
2024-04-23 22:15:02 -0500

    metodo listar

--- aplicacion/src/main/java/com/controllers/MovieControllers.java
+++ aplicacion/src/main/java/com/controllers/MovieControllers.java
@@ -1,6 +1,13 @@
 package com.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.models.Movie;
+import com.repository.MovieRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController //controlador que va a estar destinado a resolver las peticiones en formato api rest
@@ -8,4 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class MovieControllers {
     
+    @Autowired//cablear para evitar la generacion de nuevos objetos 
+    private MovieRepository movieRepository;
+
+    //metodo para listar todas las peliculas
+    public List<Movie> getAllMovie(){
+        return movieRepository.findAll();
+    }
 }
