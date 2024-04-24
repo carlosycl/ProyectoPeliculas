8f6a2a0888c60a81e93d81c6a15fc53a5609128a
carlosycl <cycortes1701@gmail.com>
2024-04-23 23:04:44 -0500

    Busqueda de movie por id

--- aplicacion/src/main/java/com/controllers/MovieControllers.java
+++ aplicacion/src/main/java/com/controllers/MovieControllers.java
@@ -6,8 +6,12 @@ import com.models.Movie;
 import com.repository.MovieRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController //controlador que va a estar destinado a resolver las peticiones en formato api rest
@@ -19,7 +23,16 @@ public class MovieControllers {
     private MovieRepository movieRepository;
 
     //metodo para listar todas las peliculas
+    @GetMapping//la uri que se ejecuten con el verbo get retornara todo el listado del repositorio de las peliculas 
     public List<Movie> getAllMovie(){
         return movieRepository.findAll();
+    }
+    //metodo que devuelve una pelicula por medio de un id
+    @GetMapping("/{id}")//para indicar que se esta recibiendo una variable se coloca entre {}
+    //ResponseEntity: verifica si el id de la uri si existe 
+    //pathVariable permite tomar el id de la uri
+    public ResponseEntity<Movie> getMovieById( @PathVariable Long id){
+        Optional<Movie> movie = movieRepository.findById(id);
+        return movie.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
     }
 }
