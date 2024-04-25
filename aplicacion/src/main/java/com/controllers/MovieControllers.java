c173b7db4d09f16fb7cf862e52e263f45e767878
carlosycl <cycortes1701@gmail.com>
2024-04-24 22:25:15 -0500

    documentacion clase controllers

--- aplicacion/src/main/java/com/controllers/MovieControllers.java
+++ aplicacion/src/main/java/com/controllers/MovieControllers.java
@@ -14,25 +14,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController //controlador que va a estar destinado a resolver las peticiones en formato api rest
-@RequestMapping("api/movies") //mepea todas la peticiones que llegan al sitio api movies 
+/*
+ * @RestController: controlador que va a estar destinado a resolver las peticiones en formato api rest
+ * @RequestMapping: mepea todas la peticiones que llegan al sitio api movies 
+ */
+
+@RestController 
+@RequestMapping("api/movies")  
 
 public class MovieControllers {
     
-    @Autowired//cablear para evitar la generacion de nuevos objetos 
+    /*@Autowired: cablear para evitar la generacion de nuevos objetos
+     *metodo para listar todas las peliculas
+     *@GetMapping: la uri que se ejecuten con el verbo get retornara todo el listado del repositorio de las peliculas
+     */
+
+    @Autowired
     private MovieRepository movieRepository;
 
-    //metodo para listar todas las peliculas
-    @GetMapping//la uri que se ejecuten con el verbo get retornara todo el listado del repositorio de las peliculas 
+    @GetMapping//  
     public List<Movie> getAllMovie(){
         return movieRepository.findAll();
     }
-    //metodo que devuelve una pelicula por medio de un id
-    @GetMapping("/{id}")//para indicar que se esta recibiendo una variable se coloca entre {}
-    //ResponseEntity: verifica si el id de la uri si existe 
-    //pathVariable permite tomar el id de la uri
+
+    /*metodo que devuelve una pelicula por medio de un id
+     * @GetMapping("/{id}"): para indicar que se esta recibiendo una variable se coloca entre {}
+     * ResponseEntity: verifica si el id de la uri si existe 
+     * pathVariable: indica que el parámetro id se extrae de la ruta de la URL. Por ejemplo, /movies/5, el valor 5 se asignará al parámetro id.
+     * Optional<Movie> para encapsular el resultado de una operación que podría no tener un valor.
+     * movieRepository.findById: busca una película por su ID usando movieRepository, que es un objeto que interactúa con la base de datos. 
+       El resultado se almacena en la variable movie, que es de tipo Optional<Movie>.
+     * orElseGet: Si no se encuentra la película (es decir, el Optional está vacío), se usa orElseGet para especificar una respuesta alternativa. 
+       Aquí, se usa para devolver ResponseEntity.notFound().build(), lo que indica un estado HTTP 404 (no encontrado).
+     */
+    
+    @GetMapping("/{id}") 
+    //
     public ResponseEntity<Movie> getMovieById( @PathVariable Long id){
+        
         Optional<Movie> movie = movieRepository.findById(id);
         return movie.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
+        // 
     }
 }
